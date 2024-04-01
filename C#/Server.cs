26b00a09a84e4e7980328b89e499f22a0a8415b0
Sam <sam@moocows.co.uk>
2024-04-01 21:22:33 +0100

    Did stuff (outdated)

--- C#/Server.cs
+++ C#/Server.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Net.Sockets
 
 namespace Server {
     class Program {
         static void Main(string[] args) {
-            
+            IPAddress ipAddress = "127.0.0.1"
+            IPEndPoint iPEndPoint = new(ipAddress, 11_000)
         }
     }
 }
