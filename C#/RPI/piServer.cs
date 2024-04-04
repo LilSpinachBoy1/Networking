f924c57f6af8daf99c1386f54c5380341bb2777c
LilSPinachBoy1 <sam@moocows.co.uk>
2024-04-04 22:26:06 +0100

    Basically finished server 💪💪

--- C#/RPI/piServer.cs
+++ C#/RPI/piServer.cs
@@ -3,27 +3,12 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
 
-namespace piServer {
-    class Program {
-        static void Main(string[] args) {
-            // Set up and start server
-            IPAddress ipAddress = IPAddress.Parse("10.10.5.12");
-            TcpListener server = new TcpListener(ipAddress, 8080);
-            server.Start();
-            Console.WriteLine("Started server on " + server.LocalEndpoint());
-
-            // Accept 2 connections
-            Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("Accepted connection!"); Console.ForegroundColor = ConsoleColor.White;
-            TcpClient c1 = server.AcceptTcpClient();
-            NetworkStream c1_stream = c1.GetStream();
-
-
-            Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("Accepted connection 2!"); Console.ForegroundColor = ConsoleColor.White;
-            TcpClient c2 = server.AcceptTcpClient();
-            NetworkStream c2_stream = c2.GetStream();
-        }
-
-        public void SendMessage(string message, NetworkStream stream) {
+namespace piServer
+{
+    class Util
+    {
+        public void SendMessage(string message, NetworkStream stream)
+        {
             // Convert message to bytes
             byte[] outBuffer = Encoding.ASCII.GetBytes(message);
 
@@ -35,7 +20,8 @@ namespace piServer {
             Console.WriteLine(message + "\n-----------------------------------");
         }
 
-        public string RecieveMessage(NetworkStream stream) {
+        public string RecieveMessage(NetworkStream stream)
+        {
             // Listen for incoming message
             byte[] inBuffer = new byte[1024];
             int inBytes = stream.Read(inBuffer, 0, inBuffer.Length);
@@ -49,6 +35,53 @@ namespace piServer {
 
             // Return decoded message
             return message;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args) {
+            // Set up and start server
+            IPAddress ipAddress = IPAddress.Parse("10.10.5.12");
+            TcpListener server = new TcpListener(ipAddress, 8080);
+            server.Start();
+            Console.WriteLine("Started server on " + server.LocalEndpoint());
+
+            // Accept connection 1
+            Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("Accepted connection!"); Console.ForegroundColor = ConsoleColor.White;
+            TcpClient c1 = server.AcceptTcpClient();
+            NetworkStream c1_stream = c1.GetStream();
+
+            // Accept connection 2
+            Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("Accepted connection 2!"); Console.ForegroundColor = ConsoleColor.White;
+            TcpClient c2 = server.AcceptTcpClient();
+            NetworkStream c2_stream = c2.GetStream();
+
+            // NOTE: This code probably wont work, but we'll give it a go anyway...
+            // Might need to put it in a while loop to keep it running if it only does one message
+
+            Task.Run(() =>
+            {
+                // Async task for CLIENT 1
+                // Listen for messages from client 2 and echo to client 1
+                string msg = Util.RecieveMessage(c2_stream);
+                Util.SendMessage(msg, c1_stream);
+            });
+
+            Task.Run(() =>
+            {
+                // Async task for CLIENT 2
+                // Listen for messages from client 1 and echo to client 2
+                string msg = Util.RecieveMessage(c1_stream);
+                Util.SendMessage(msg, c2_stream);
+            });
+
+            // Close down shiz
+            c1_stream.Close();
+            c2_stream.Close();
+            c1.Close();
+            c2.Close();
+            server.Close();
         }
     }
 }
